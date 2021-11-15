8fa91aef0d8d05d9de63b4141af40ced171fa0d9
undefined <armyman6549@protonmail.com>
2021-11-15 15:38:57 +0000

    a

--- app.ts
+++ app.ts
@@ -1,7 +1,23 @@
 import express from 'express';
-
+import formidable from 'formidable';
+import db from 'quick.db';
 const app = express();
 const port = 3000;
+
+app.post('/', (req, res) => {
+var form = new formidable.IncomingForm()
+form.parse(req, function (fields) {
+    console.log(fields)
+    var url = fields["url"]
+    var key = fields["key"]
+    
+    if(!key) {
+        res.status(400).send({error: 'Include a key'})
+    }
+
+    })
+    
+})
 
 app.listen(port, () => {
     console.log('app running on {port}')
